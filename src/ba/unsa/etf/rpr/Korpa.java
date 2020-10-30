626779723a04725ff67cb3c5e8b93c85a052a19d
nsubasic1 <nsubasic1@etf.unsa.ba>
2020-10-30 17:07:28 +0100

    uradjen

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int vel = 0;
+
+    public boolean dodajArtikl(Artikl artikl){
+        if (vel >= 50) return false;
+        artikli[vel] = artikl;
+        vel = vel + 1;
+        return true;
+    }
+
+    public Artikl[] getArtikli(){
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod){
+        for(int i=0; i < vel; i++){
+            int temp = Integer.parseInt(artikli[i].getKod());
+            if(temp == Integer.parseInt(kod)){
+                Artikl zaVracanje = artikli[i];
+                artikli[i] = null;
+                System.arraycopy(artikli, i+1, artikli, i, vel - i);
+                vel = vel - 1;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala(){
+        int ukupno = 0;
+        for(int i = 0; i < vel; i++){
+            ukupno = ukupno + artikli[i].getCijena();
+        }
+        return ukupno;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private int vel = 0;
+
+    public void dodajArtikl(Artikl a){
+        artikli[vel] = a;
+        vel = vel + 1;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        for(int i=0; i < vel; i++){
+            int temp = Integer.parseInt(artikli[i].getKod());
+            if(temp == Integer.parseInt(kod)){
+                Artikl zaVracanje = artikli[i];
+                artikli[i] = null;
+                System.arraycopy(artikli, i+1, artikli, i, vel - i);
+                vel = vel - 1;
+            }
+        }
+        return null;
+    }
+
+    public int getVel() {
+        return vel;
+    }
 }
